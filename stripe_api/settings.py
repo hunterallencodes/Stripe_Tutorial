61ab6bebc693379462fe43b9ef7803c150badf42
snake-whisperer <79474323+snake-whisperer@users.noreply.github.com>
2021-07-11 16:18:50 -0500

    Fourth commit

--- stripe_api/settings.py
+++ stripe_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,5 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
+    "http://localhost:8005",
 ]
